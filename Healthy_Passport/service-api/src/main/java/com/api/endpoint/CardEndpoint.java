48dc77a854ec3b7172d10feaaba5e31c126f36d4
Ярослав <kaon1999@gmail.com>
2018-12-22 22:38:30 +0200

    Созданы респонсы и кардЕндпоинт
    20.12.2018

---
+++ Healthy_Passport/service-api/src/main/java/com/api/endpoint/CardEndpoint.java
@@ -0,0 +1,49 @@
+package com.api.endpoint;
+
+
+import com.api.response.GeneralErrorResponse;
+import com.api.response.GeneralResponse;
+import io.swagger.annotations.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@SwaggerDefinition(info = @Info(description = "Ampula API ", version = "1.0.0", title = "Ampula API"),
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        schemes = SwaggerDefinition.Scheme.HTTPS
+)
+@Api(tags = "Card Service ", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,protocols = "https")
+@RequestMapping(value = "api/card/")
+
+public interface CardEndpoint {
+    @ApiOperation(value = "Create new card for patient")
+    @ApiResponse({
+            @ApiResponse(code = 200, message = "OK - Card record is created"),
+            @ApiResponse(code = 500, message = "Fail - Internal server error",response = GeneralErrorResponse.class),
+    })
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    GeneralResponse<Long> createCard ()
+
+    /*
+    *
+    *
+    * @SwaggerDefinition(
+        info = @Info(description = "Management API", version = "1.0.0", title = "Management API"),
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        schemes = SwaggerDefinition.Scheme.HTTPS
+)
+@Api(tags = "Account Service", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, protocols = "https")
+@RequestMapping(value = "management/account")
+public interface AccountEndpoint {
+
+    @ApiOperation(value = "Create new account for client record")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK - Account record is created"),
+            @ApiResponse(code = 500, message = "FAIL - Internal server error",
+                    response = GeneralErrorResponse.class)
+    })
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    GeneralResponse<Long> createAccount(GeneralRequest<Void, AccountParametrs> accountRequest);
+    * */
+}
