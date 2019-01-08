70b73dbf17b1c096358572a11c495ff6100af390
Yaroslav <kaon1999@gmail.com>
2019-01-08 21:51:45 +0200

    1

--- Healthy_Passport/service-core/src/main/java/com/service/core/config/SwaggerConfig.java
+++ Healthy_Passport/service-core/src/main/java/com/service/core/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
+   @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
