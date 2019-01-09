578c8b2dfe18370c84b89efed001638f31336ea9
Aleks1696 <aleks.borodavka@ukr.net>
2019-01-09 20:59:12 +0200

    Doctor entity update

--- Healthy_Passport/service-core/src/main/java/com/service/core/domain/Doctor.java
+++ Healthy_Passport/service-core/src/main/java/com/service/core/domain/Doctor.java
@@ -3,7 +3,9 @@ package com.service.core.domain;
 
 
 
-import com.api.request.DoctorsTypes;
+import com.api.request.types.DoctorsTypes;
+import com.service.core.dao.CardRepository;
+import com.service.core.dao.DoctorRepository;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -17,6 +19,9 @@ import java.util.List;
  * 3.Какой врач(Хирург,кардиолог и тд)
  *
  * 20.12.2018 - by Yaroslav Halindzovskiy
+ *
+ * Aleksandr Borodavka 08.01.2019
+ * добавил анотацию @Enumarated(EnumType.STRING)
  * */
 @Entity
 @Table(name = "doctors")
@@ -31,23 +36,37 @@ public class Doctor {
     @Column
     private String surname;
     @Column
+    /*Нужна для того чтобы в базе сохранялись енами в виде
+     строки (если доктор например SURGEON (№1 в списке енамов) то по умолчанию он сохранится в базе как 1.
+     С этой анотацией в базе будет хранится не номер, а стока SURGEON.*/
+    @Enumerated(EnumType.STRING)
     private DoctorsTypes doctorsTypes;
 
 
-    @ManyToMany(fetch = FetchType.EAGER, targetEntity = Patient.class)
-    @JoinTable(name = "record_on_patients",
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = Card.class)
+    @JoinTable(name = "Doctor_Card",
             joinColumns = @JoinColumn(name = "doctors_ref_id", referencedColumnName = "doctor_id"),
-            inverseJoinColumns = @JoinColumn(name = "patient_ref_id", referencedColumnName = "patient_id"))
-    private List<Patient> patients = new ArrayList<Patient>();
+            inverseJoinColumns = @JoinColumn(name = "card_ref_id", referencedColumnName = "card_id"))
+    private List<Card> cards = new ArrayList<Card>();
 
     public Doctor() {
     }
 
-    public Doctor(String name, String surname, DoctorsTypes doctorsTypes, List<Patient> patients) {
+    public Doctor(String name, String surname, DoctorsTypes doctorsTypes, List<Card> patients) {
         this.name = name;
         this.surname = surname;
         this.doctorsTypes = doctorsTypes;
-        this.patients = patients;
+        this.cards = patients;
+    }
+
+    //костылизм
+    //если нужно удалить доктора этот метод удаляет из каждой карты данного доктора
+    public void removeDoctor(DoctorRepository doctorRepository, CardRepository cardRepository){
+        for (Card card : cards){
+            card.getDoctors().remove(this);
+            cardRepository.saveAndFlush(card);
+        }
+        doctorRepository.saveAndFlush(this);
     }
 
     public Long getId() {
@@ -82,12 +101,12 @@ public class Doctor {
         this.doctorsTypes = doctorsTypes;
     }
 
-    public List<Patient> getPatients() {
-        return patients;
+    public List<Card> getCards() {
+        return cards;
     }
 
-    public void setPatients(List<Patient> patients) {
-        this.patients = patients;
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
     }
 
     @Override
@@ -97,7 +116,7 @@ public class Doctor {
         sb.append(", name='").append(name).append('\'');
         sb.append(", surname='").append(surname).append('\'');
         sb.append(", doctorsTypes=").append(doctorsTypes);
-        sb.append(", patients=").append(patients);
+        sb.append(", cards=").append(cards);
         sb.append('}');
         return sb.toString();
     }
