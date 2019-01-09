6e842ddfffb0e9185d77c82727f9b1223785b1f9
Aleks1696 <aleks.borodavka@ukr.net>
2019-01-09 21:17:23 +0200

    WebConfig is commented (excluded)

--- Healthy_Passport/service-core/src/main/java/com/service/core/config/WebConfig.java
+++ Healthy_Passport/service-core/src/main/java/com/service/core/config/WebConfig.java
@@ -1,20 +1,20 @@
-package com.service.core.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/lib/**")
-                .addResourceLocations("classpath:/static/lib/");
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
-    }
-
-}
+//package com.service.core.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+////@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/lib/**")
+//                .addResourceLocations("classpath:/static/lib/");
+//        registry.addResourceHandler("/js/**")
+//                .addResourceLocations("classpath:/static/js/");
+//    }
+//
+//}
