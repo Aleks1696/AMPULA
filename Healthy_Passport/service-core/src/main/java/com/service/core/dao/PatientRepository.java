23c18412142452dff747c5e3fe38bf9aff34ee93
Aleks1696 <aleks.borodavka@ukr.net>
2019-01-09 21:16:29 +0200

    Patient repository (method to check whether user is already exist in DB)

--- Healthy_Passport/service-core/src/main/java/com/service/core/dao/PatientRepository.java
+++ Healthy_Passport/service-core/src/main/java/com/service/core/dao/PatientRepository.java
@@ -4,4 +4,7 @@ import com.service.core.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    
+    Patient findByNameAndSurnameEquals(String name, String surname);
+    
 }
